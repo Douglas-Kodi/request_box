27224c36cea7602bf181e68e8a2b38cce75028a2
Douglas Kodi <kodi.shimoura@gmail.com>
2022-06-30 18:20:46 -0400

    Add Navbar and Footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,23 @@
+<template>
+    <footer id="footer">
+        <p>Required Box &copy 2022</p>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: "Footer"
+}
+</script>
+
+<style scoped>
+    #footer{
+        width:100%;
+        height:200px;
+        background-color:rgb(134, 130, 130);
+        color: black;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,45 @@
+<template>
+    <div id="nav">
+        <router-link to="/" id="logo-url">
+            <img :src="logo" :alt="alt" id="logo">
+        </router-link>
+        <router-link to="/">Home</router-link>
+        <router-link to="/List">List</router-link>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Navbar",
+    props: ["logo", "alt"]
+}
+</script>
+
+<style scoped>
+    #nav{
+        width:100%;
+        height:75px;
+        background-color:rgb(134, 130, 130);
+        display:flex;
+        justify-content:flex-end;
+        align-items:center;
+    }
+    #nav a{
+        padding:0px 20px;
+        font-size:20px;
+        color:black;
+        transition:.5s;
+    }
+    #nav a:hover{
+        color:white;
+    }
+    #logo-url{
+        margin:auto;
+        margin-left:0;
+    }
+    #logo{
+        width:70px;
+        height:70px;
+        padding:5px;
+    }
+</style>
